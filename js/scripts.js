c87d55e29252557ea7b0b503d7f1677b8e8613ed
Carlo De Nardin <carlodenardin@Air-di-Carlo.station>
2022-12-06 15:45:29 +0100

    Added some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -10,25 +10,25 @@
 window.addEventListener('DOMContentLoaded', event => {
 
     // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
+    const sideNav = document.body.querySelector('#sideNav') ;
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
             offset: 74,
-        });
-    };
+        }) ;
+    }!important ;
 
     // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const navbarToggler = document.body.querySelector('.navbar-toggler') ;
     const responsiveNavItems = [].slice.call(
         document.querySelectorAll('#navbarResponsive .nav-link')
-    );
+    ) ;
     responsiveNavItems.map(function (responsiveNavItem) {
         responsiveNavItem.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
+                navbarToggler.click() ;
             }
-        });
-    });
+        }) ;
+    }) ;
 
 });
